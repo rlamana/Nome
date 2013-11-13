559b08010a5b52aec978e645093adee4b30bc8be
Ramón Lamana <rlamana@gmail.com>
2013-11-13 00:19:52 -0800

    Fixes client code

--- build/dist/nome.js
+++ build/dist/nome.js
@@ -1,10 +1,97 @@
 /*! nome - v0.0.1 - 2013-11-13
 * Copyright (c) 2013 Ramon Lamana; Licensed MIT */
 define("client", 
-  [],
-  function() {
+  ["emitter","extend"],
+  function(__dependency1__, __dependency2__) {
     "use strict";
+    var Emitter = __dependency1__["default"];
+    var extend = __dependency2__["default"];
 
+    var WebSocket = require('ws');
+
+    var Monome = function() {
+            Emitter.apply(this);
+
+            this.connected = false;
+    };
+
+    Monome.prototype = extend(Emitter.prototype, {
+            get connected() {
+                    return this._connected;
+            },
+
+            set connected(value) {
+                    if(typeof this._connected === 'undefined') {
+                            this._connected = value;
+                    }
+            },
+            
+            connect: function(host, port) {
+                    var websocket;
+                    port = port || '5555';
+                    host = host || '127.0.0.1';
+
+                    websocket = this._websocket = new WebSocket('ws://' + host + ':' + port);
+
+                    websocket.on('error', function(){
+                            console.error('Could not connected to Monode server @ ' + host + ':' + port);
+                    });
+
+                    websocket.on('open', function() {
+                            this._connected = true;
+                            console.log('Connected to Monode server @ ' + host + ':' + port);
+                            this.emit('connected');
+                    }.bind(this));
+
+                    websocket.on('message', function(data) {
+                            data = JSON.parse(data);
+
+                            // @todo Check if event is in a list of valid events
+                            if(data.event) {
+                                    this.emit.apply(this, [data.event].concat(data.args));
+                            }
+                    }.bind(this));
+
+                    return this;
+            },
+
+            led: function(x, y, s) {
+                    if(this.connected) {
+                            this._websocket.send(JSON.stringify({
+                                    method: 'led',
+                                    args: [x,y,s]
+                            }));
+                    }
+                    else {
+                            console.warn('led: Not connected to any Monode server');
+                    }
+            },
+
+            _websocket: null
+    });
+
+
+    ///////// example
+
+    var monome = new Monome();
+    monome.connect().on('connected', function() {
+
+    });
+
+    var grid = {};
+
+    monome.on('key', function(x,y,s) {
+            var id = x+''+y;
+            if(s) {
+                    console.log('KEY: ', x, y);
+                    if(!grid[id]) {
+                            grid[id] = 0;
+                    }
+
+                    grid[id] = !grid[id];
+                    monome.led(x, y, grid[id]);
+            }
+    });
   });
 define("emitter", 
   ["exports"],
---
+++ src/client/client.js
@@ -0,0 +1,88 @@
+import Emitter from 'emitter';
+import extend from 'extend';
+
+var WebSocket = require('ws');
+
+var Monome = function() {
+        Emitter.apply(this);
+
+        this.connected = false;
+};
+
+Monome.prototype = extend(Emitter.prototype, {
+        get connected() {
+                return this._connected;
+        },
+
+        set connected(value) {
+                if(typeof this._connected === 'undefined') {
+                        this._connected = value;
+                }
+        },
+        
+        connect: function(host, port) {
+                var websocket;
+                port = port || '5555';
+                host = host || '127.0.0.1';
+
+                websocket = this._websocket = new WebSocket('ws://' + host + ':' + port);
+
+                websocket.on('error', function(){
+                        console.error('Could not connected to Monode server @ ' + host + ':' + port);
+                });
+
+                websocket.on('open', function() {
+                        this._connected = true;
+                        console.log('Connected to Monode server @ ' + host + ':' + port);
+                        this.emit('connected');
+                }.bind(this));
+
+                websocket.on('message', function(data) {
+                        data = JSON.parse(data);
+
+                        // @todo Check if event is in a list of valid events
+                        if(data.event) {
+                                this.emit.apply(this, [data.event].concat(data.args));
+                        }
+                }.bind(this));
+
+                return this;
+        },
+
+        led: function(x, y, s) {
+                if(this.connected) {
+                        this._websocket.send(JSON.stringify({
+                                method: 'led',
+                                args: [x,y,s]
+                        }));
+                }
+                else {
+                        console.warn('led: Not connected to any Monode server');
+                }
+        },
+
+        _websocket: null
+});
+
+
+///////// example
+
+var monome = new Monome();
+monome.connect().on('connected', function() {
+
+});
+
+var grid = {};
+
+monome.on('key', function(x,y,s) {
+        var id = x+''+y;
+        if(s) {
+                console.log('KEY: ', x, y);
+                if(!grid[id]) {
+                        grid[id] = 0;
+                }
+
+                grid[id] = !grid[id];
+                monome.led(x, y, grid[id]);
+        }
+});
